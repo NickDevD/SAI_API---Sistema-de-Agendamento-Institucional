bdf6511efbeb7200fedcc059950dc491d9ca4d14
Nicholas Monteiro <nick.developerdev@gmail.com>
2026-04-09 21:27:26 -0300

    Deploy front

--- front/src/pages/AgendamentoPage.tsx
+++ front/src/pages/AgendamentoPage.tsx
@@ -35,7 +35,7 @@ import axios from 'axios';
 
 // ---------- API ----------
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
 });
 
 api.interceptors.request.use((config) => {
