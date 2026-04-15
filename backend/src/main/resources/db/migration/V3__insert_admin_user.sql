a2743e9c24eb55e06eab0c2f979a4341deba64cf
Nicholas Monteiro <nick.developerdev@gmail.com>
2026-04-15 19:20:29 -0300

    fix user default

--- backend/src/main/resources/db/migration/V3__insert_admin_user.sql
+++ backend/src/main/resources/db/migration/V3__insert_admin_user.sql
@@ -1,5 +1,5 @@
 INSERT INTO tb_usuarios (id, login, senha, role)
-SELECT gen_random_uuid(), '$2a$10$NXivwX8RVSp7CvbZxhgNQOOQBGar5MSFH/pDAK4cnsDDpRJMUKthK', '$2a$10$NLilALcVtSApBw.AxVjotub2K9fnJz6Z981Fc42tTepGM9kNiZiBu', 'ADMIN'
+SELECT gen_random_uuid(), 'admin', '$2a$10$NLilALcVtSApBw.AxVjotub2K9fnJz6Z981Fc42tTepGM9kNiZiBu', 'ADMIN'
     WHERE NOT EXISTS (
     SELECT 1 FROM tb_usuarios WHERE login = 'admin'
 );
