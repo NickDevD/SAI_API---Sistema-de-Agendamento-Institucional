a890cbcc5bbd1bada988c317042bac7564925b09
Nicholas Monteiro <nick.developerdev@gmail.com>
2026-04-23 20:51:55 -0300

    add user default

--- backend/src/main/resources/db/migration/V3__insert_admin_user.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tb_usuarios (id, login, senha, role)
-SELECT gen_random_uuid(), '${ADMIN_LOGIN}', '${ADMIN_PASSWORD_HASH}', 'ADMIN'
-    WHERE NOT EXISTS (
-    SELECT 1 FROM tb_usuarios WHERE login = '${ADMIN_LOGIN}'
-);
